77f89dd89ec77ac037dcb8e941fc151e7805806d
Josue Joel Garcia Abata <josuegarcab2@hotmail.com>
2026-01-25 17:03:21 -0500

    delete oauth2 server, client an resourcer servers. they dont work well

--- gateway-server/src/main/java/com/josue/gateway_server/configs/SecurityConfig.java
+++ gateway-server/src/main/java/com/josue/gateway_server/configs/SecurityConfig.java
@@ -2,45 +2,22 @@ package com.josue.gateway_server.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import reactor.core.publisher.Mono;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        return http.authorizeExchange(authz ->
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz ->
                         authz
-                        .pathMatchers("/authorized", "/logout").permitAll()
-                        .pathMatchers("/api/v1/items/**", "/api/v1/users/**", "/api/v1/products/**").hasAnyRole("ADMIN", "USER")
-                        .anyExchange().authenticated()
-                ).cors(ServerHttpSecurity.CorsSpec::disable)
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-                .oauth2Login(Customizer.withDefaults())
-                .oauth2Client(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
-                        jwtSpec -> jwtSpec.jwtAuthenticationConverter(
-                                (Converter<Jwt, Mono<AbstractAuthenticationToken>>) source -> {
-                                    Collection<String> roles = source.getClaimAsStringList("roles");
-                                    Collection<GrantedAuthority> authorities = roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-                                    return Mono.just(new JwtAuthenticationToken(source, authorities));
-                                }
-                        )
-                ))
+                        .requestMatchers("/authorized", "/logout", "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
